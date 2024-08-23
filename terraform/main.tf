2e9c4e639814bb6193c69c78660c7c8ebdff4ec0
AM7754 <am7754@zebra.com>
2024-08-23 10:43:47 +0530

    Maven Changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,8 @@ provider "google" {
 }
 
 resource "google_container_cluster" "primary" {
-  name     = var.cluster_name
-  location = var.region
+  name               = var.cluster_name
+  location           = var.region
   initial_node_count = var.node_count
 
   node_config {
