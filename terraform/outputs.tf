379223e02513d6d9ca9926b72c1815bcc79c06ef
AM7754 <am7754@zebra.com>
2024-08-23 14:18:08 +0530

    Deploymment 4

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "kubeconfig" {
   description = "The kubeconfig file for accessing the Kubernetes cluster."
-  value       = google_container_cluster.primary.endpoint
+  value       = google_container_cluster.my-gke-cluster.endpoint
 }
