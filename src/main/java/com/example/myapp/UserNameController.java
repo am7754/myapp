9f72dbda443b138de699eea29e43a16ae82fdc65
AM7754 <am7754@zebra.com>
2024-08-27 19:07:45 +0530

    MySQL Changes- 8

--- src/main/java/com/example/myapp/UserNameController.java
+++ src/main/java/com/example/myapp/UserNameController.java
@@ -1,21 +1,28 @@
 package com.example.myapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
 public class UserNameController {
 
-    @Autowired
-    private UserRepository repository;
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserNameController(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @GetMapping
-    public List<User> getAllEntities() {
-        return repository.findAll();
+    public List<String> getAllEntities() {
+        return this.jdbcTemplate.queryForList("SELECT * FROM users").stream()
+                .map(m -> m.values().toString())
+                .collect(Collectors.toList());
     }
 }
